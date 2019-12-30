96a140b4908bc344214198e5c1aeccbd208a692c
Sternchen-xnike <59176786+Sternchen-xnike@users.noreply.github.com>
2019-12-30 13:21:53 +0100

    Update and rename Benutzeroberfläche 23.12.java to mainClass.java

---
+++ mainClass.java
@@ -0,0 +1,13 @@
+
+
+public class mainClass {
+
+public static void main (String [] args) {
+	
+	mainWindow myMainWindow = new mainWindow ("penfactory"); 
+	productView myProductView = new productView ("Produktansicht: "); 
+	
+	} // public static void main
+
+} // public class mainClass
+
