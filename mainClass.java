1ac24545e6b0eedd3f8d025dcfd45a345476a611
Sternchen-xnike <59176786+Sternchen-xnike@users.noreply.github.com>
2020-01-03 11:10:32 +0100

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -1,13 +1,13 @@
 
-
-public class mainClass {
+public class mainClass2 {
 
 public static void main (String [] args) {
 	
-	mainWindow myMainWindow = new mainWindow ("penfactory"); 
-	productView myProductView = new productView ("Produktansicht: "); 
+	ProductView1 myProductView = new ProductView1 (); 
+	addProduct1 myProduct1 = new addProduct1 ();
+	category1 myCategory1 = new category1 ();
+	mainWindow1 myMainWindow1 = new mainWindow1 ();
 	
 	} // public static void main
 
 } // public class mainClass
-
